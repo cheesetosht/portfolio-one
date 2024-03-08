99485a764d2f0401ba9ad8c1eff36d6b31ec7bbf
Vraj Shah <vraj@bluelearn.in>
2024-03-08 19:10:43 +0530

    add tooltips to social links

--- src/components/social-links.svelte
+++ src/components/social-links.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Icon from '@iconify/svelte';
 
-	const email = `mailto:${import.meta.env.VITE_EMAIL}`;
+	const email = import.meta.env.VITE_EMAIL;
 	const github = 'https://www.github.com/cheesetosht';
 	const linkedin = 'https://www.linkedin.com/in/cheesetosht';
 	const instagram = 'https://www.instagram.com/cheesetosht';
@@ -9,20 +9,19 @@
 </script>
 
 <div class="flex gap-2 items-center justify-end">
-	<a target="_blank" href={email}>
+	<a title={`say ${email}`} target="_blank" href={`mailto:${email}`}>
 		<Icon class="social-icon" icon="ph:envelope-simple-open-duotone" />
 	</a>
 	<!-- <a target="_blank" href={instagram}>
 		<Icon class="social-icon" icon="ph:instagram-logo-duotone" />
 	</a> -->
-	<a target="_blank" href={linkedin}>
+	<a title="linkedin" target="_blank" href={linkedin}>
 		<Icon class="social-icon" icon="ph:linkedin-logo-duotone" />
 	</a>
-	<a target="_blank" href={twitter}>
+	<a title="the bird app" target="_blank" href={twitter}>
 		<Icon class="social-icon" icon="ph:twitter-logo-duotone" />
 	</a>
-
-	<a target="_blank" href={github}>
+	<a title="github" target="_blank" href={github}>
 		<Icon class="social-icon" icon="ph:github-logo-duotone" />
 	</a>
 </div>
