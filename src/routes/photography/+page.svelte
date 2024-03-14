dc90c07da5d606cf9780e7508dc86b69ea4a11b2
Vraj Shah <vraj@bluelearn.in>
2024-03-15 02:54:45 +0530

    some files got left out

---
+++ src/routes/photography/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { PHOTOS } from '../../lib/constants';
+</script>
+
+<h1 class="page-heading">photography</h1>
+<p class="text-sm mb-6">aesthetik</p>
+
+<div class="columns-2 lg:columns-3">
+	{#each PHOTOS as photo}
+		<img class="mb-6" loading="eager" src={`https://${photo}?w=160`} alt="" />
+	{/each}
+</div>
