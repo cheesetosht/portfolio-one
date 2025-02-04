777b0f55002fcb6d70ed383ebd9b232856cac440
Vraj Shah <cheesetosht@gmail.com>
2025-02-05 02:57:30 +0530

    v2: prepare base grid layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,8 @@
 <script>
-	// @ts-nocheck
 	import '@fontsource/manrope';
 	import '../font.css';
 	import '../app.css';
-	import Nav from '../components/nav.svelte';
-	import Footer from '../components/footer.svelte';
-	import { onMount } from 'svelte';
 	import { MetaTags } from 'svelte-meta-tags';
-
-	const MS_CLARITY_ID = import.meta.env.VITE_MS_CLARITY_ID;
-	onMount(() => {
-		(function (c, l, a, r, i, t, y) {
-			c[a] =
-				c[a] ||
-				function () {
-					(c[a].q = c[a].q || []).push(arguments);
-				};
-			t = l.createElement(r);
-			t.async = 1;
-			t.src = 'https://www.clarity.ms/tag/' + i;
-			y = l.getElementsByTagName(r)[0];
-			y.parentNode.insertBefore(t, y);
-		})(window, document, 'clarity', 'script', MS_CLARITY_ID);
-	});
 </script>
 
 <MetaTags
@@ -36,20 +16,6 @@
 	}}
 	keywords={['cheesetosht', 'cheesetoast', 'vraj shah', 'cheese toast']}
 />
-<div class="transition-all duration-200 container">
-	<div class="grid grid-cols-4 items-start gap-x-5 lg:grid-cols-12">
-		<Nav class="col-span-4 lg:col-span-2 lg:col-start-2" />
-		<main
-			id="content"
-			class="relative col-span-4 col-start-1 lg:col-start-5 lg:pt-24 pb-6 lg:col-span-6"
-		>
-			<slot />
-		</main>
-		<Footer class="lg:hidden col-span-4 col-start-1" />
-		<!-- <div
-			class="col-span-4 col-start-1 lg:col-start-5 text-center lg:text-left text-gray-400 text-xs pb-4 lg:col-span-6"
-		>
-			built with <strong>svelte<Icon class="inline-block" icon="carbon:logo-svelte" /></strong>
-		</div> -->
-	</div>
+<div class="transition-all duration-200 max-w-screen-sm mx-auto">
+	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,94 @@
 <script>
 	import { MetaTags } from 'svelte-meta-tags';
-	import Icon from '@iconify/svelte';
 </script>
 
 <MetaTags title="cheesetosht" />
-<!-- <Icon class="h-14 w-14 mb-8 rotate-45" icon="ic:baseline-bug-report" /> -->
-<svg
-	xmlns="http://www.w3.org/2000/svg"
-	aria-hidden="true"
-	role="img"
-	class="h-14 w-14 mb-8 rotate-45"
-	width="1em"
-	height="1em"
-	viewBox="0 0 24 24"
-	><path
-		fill="currentColor"
-		d="M20 8h-2.81a5.985 5.985 0 0 0-1.82-1.96L17 4.41L15.59 3l-2.17 2.17C12.96 5.06 12.49 5 12 5c-.49 0-.96.06-1.41.17L8.41 3L7 4.41l1.62 1.63C7.88 6.55 7.26 7.22 6.81 8H4v2h2.09c-.05.33-.09.66-.09 1v1H4v2h2v1c0 .34.04.67.09 1H4v2h2.81c1.04 1.79 2.97 3 5.19 3s4.15-1.21 5.19-3H20v-2h-2.09c.05-.33.09-.66.09-1v-1h2v-2h-2v-1c0-.34-.04-.67-.09-1H20zm-6 8h-4v-2h4zm0-4h-4v-2h4z"
-	></path></svg
->
-<h1 class="font-display font-bold text-xl group lg:text-3xl select-none cursor-default mb-4">
-	i'm vraj, an engineer. <br />i like my tech crafted than built.
-</h1>
-<!-- <h1 class="font-display font-bold text-xl group lg:text-3xl select-none cursor-default mb-6">
-	en<span class="inline-block transition duration-200 lg:-rotate-12 lg:group-hover:-rotate-0"
-		>g</span
-	>ineering
-	<br class="xl:hidden" />
-	unreason<span
-		class="inline-block transition origin-left duration-200 lg:rotate-12 lg:group-hover:rotate-0"
-		>a</span
-	>blely
-</h1> -->
 
-<!-- <h4 class="font-semibold text-base font-display">currently:</h4> -->
-<!-- <p class="text-sm mb-4">
-	currently: exploring.
-</p> -->
-<p class="text-sm mb-4">
-	i like to play around with linuxes, computer networks, frontends and databases.
-</p>
-<p class="text-sm mb-4">overengineering is my goto way of learning.</p>
-<h5
-	class="font-display font-bold group text-base lg:text-lg select-none cursor-default mb-1 text-gray-500"
->
-	about this site
-</h5>
-<p class="text-sm mb-4">
-	this site is built on <a href="https://kit.svelte.dev/">sveltekit</a> and
-	<a href="https://tailwindcss.com/">tailwind</a>. the
-	<a href="/photography">photography section</a>
-	uses data from a small
-	<a href="https://github.com/cheesetosht/go-scripts/blob/main/crawlers/main.go">golang script</a>
-	i wrote to fetch all the photos I post on my <a href="https://vsco.co/cheesetosht">VSCO</a>.
-</p>
+<main class="grid" id="home_grid">
+	<div id="eyes"></div>
+	<div id="logo"></div>
+	<div id="about"></div>
+	<div id="intro"></div>
+	<div id="projects"></div>
+	<div id="work"></div>
+	<div id="blogs"></div>
+	<div id="music"></div>
+	<div id="socials"></div>
+	<div id="other"></div>
+</main>
 
-<!-- TODO: hobbies section -->
+<style>
+	#home_grid {
+		display: grid;
+		padding: 24px;
+		grid-template-areas:
+			'eyes logo about'
+			'projects intro about'
+			'projects music work'
+			'socials blogs other';
+	}
+	#home_grid div {
+		border: 1px black solid;
+		margin-top: -1px;
+		margin-left: -1px;
+		margin-bottom: -1px;
+	}
+	#eyes {
+		grid-area: eyes;
+		aspect-ratio: 1/1;
+	}
+	#logo {
+		grid-area: logo;
+		aspect-ratio: 1/1;
+		background: #ec5751;
+	}
+	#about {
+		grid-area: about;
+		aspect-ratio: 1/2;
+		background: #3e6917;
+	}
+	#intro {
+		grid-area: intro;
+		aspect-ratio: 1/1;
+		background: #98d3d9;
+	}
+	#projects {
+		grid-area: projects;
+		aspect-ratio: 1/2;
+		background: #ff9ef3;
+	}
+	#work {
+		grid-area: work;
+		aspect-ratio: 1/1;
+		background: #fff16c;
+	}
+	#blogs {
+		grid-area: blogs;
+		aspect-ratio: 1/1;
+		background: #aea8d4;
+	}
+	#music {
+		grid-area: music;
+		aspect-ratio: 1/1;
+	}
+	#socials {
+		grid-area: socials;
+		aspect-ratio: 1/1;
+	}
+	#other {
+		grid-area: other;
+		aspect-ratio: 1/1;
+	}
+	@media only screen and (max-width: 600px) {
+		#home_grid {
+			height: auto;
+			grid-template-areas:
+				'eyes logo'
+				'intro music'
+				'projects blogs'
+				'projects work'
+				'socials about'
+				'other about';
+		}
+	}
+</style>
